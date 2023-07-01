d20fee08ad09b72c840a5583877e0cb4a748816e
user <user@localhost>
2023-07-01 12:34:28 +0000

    Checkpoint

--- avtdl.py
+++ avtdl.py
@@ -4,60 +4,19 @@ import asyncio
 import argparse
 import logging
 import os
-from typing import Dict, List, Optional, Tuple
 from pathlib import Path
-from pprint import pformat
 
 import yaml
 
 from plugins.core.interfaces import Action, Filter, Monitor, Record
+from plugins.core.chain import Chain
 from plugins.core.filters import MatchFilter
 from plugins.xmpp.send_jabber import SendJabber, JabberConfig, JabberEntity
 from plugins.rss.youtube_rss import FeedMonitor, FeedMonitorConfig, FeedMonitorEntity
 from plugins.execute.run_command import Command, CommandConfig, CommandEntity
 from plugins.file.text_file import FileMonitor, FileMonitorEntity, FileMonitorConfig, FileAction, FileActionEntity, FileActionConfig
+from plugins.core import config
 
-class Chain:
-    def __init__(self,
-                 monitors: List[Tuple[Monitor, List[str]]],
-                 actions: List[Tuple[Action, List[str]]],
-                 filters: Optional[List[Filter]] = None,
-                 name: str = "ChainX"):
-        self.name = name
-        self.monitors = monitors
-        self.actions = actions
-        self.filters = filters or []
-        for monitor, monitor_entities in monitors:
-            for monitor_entity in monitor_entities:
-                monitor.register(monitor_entity, self.handle)
-
-    def filter(self, record: Record):
-        for f in self.filters:
-            record = f.match(record)
-            if record is None:
-                break
-        return record
-
-    def handle(self, record: Record):
-        record = self.filter(record)
-        if record is None:
-            return
-        for action, action_entities in self.actions:
-            for action_entity_name in action_entities:
-                action.handle(action_entity_name, record)
-
-    def _pformat(self, section):
-        f = []
-        for item in section:
-            item_instance, item_entries = item
-            item_name = item_instance.__class__.__name__
-            f.append(f'{item_name}, {item_entries}')
-            return ', '.join(f)
-
-    def __repr__(self):
-        m = self._pformat(self.monitors)
-        a = self._pformat(self.actions)
-        return f'Chain("{self.name}", {m}, {self.filters!r}, {a})'
 
 def load_config(path):
     if not os.path.exists(path):
---
+++ plugins/core/config.py
@@ -0,0 +1,56 @@
+import logging
+from enum import Enum
+from typing import Dict, Tuple, List, Union
+
+from plugins.core.interfaces import Monitor, MonitorEntity, MonitorConfig, Action, ActionEntity, ActionConfig, Filter
+
+class Plugins:
+
+    class kind(Enum):
+        MONITOR = 'monitor'
+        MONITOR_CONFIG = 'monitor_config'
+        MONITOR_ENTITY = 'monitor_entity'
+        ACTION = 'action'
+        ACTION_CONFIG = 'action_config'
+        ACTION_ENTITY = 'action_entity'
+        FILTER = 'filter'
+
+    known: Dict[kind, Dict] = {k: {} for k in kind}
+
+    @classmethod
+    def _register(cls, name: str, kind, instance: Union[Monitor, MonitorEntity, MonitorConfig, Action, ActionEntity, ActionConfig, Filter]):
+        cls.known[kind][name] = instance
+
+    @classmethod
+    def _get(cls, name: str, kind: kind):
+        instance = cls.known[kind].get(name)
+        if instance is None:
+            raise KeyError(f'"{name}" is not registered as "{kind}"')
+        return instance
+
+    @classmethod
+    def get_monitor_factories(cls, name):
+        monitor_factory = cls._get(name, cls.kind.MONITOR)
+        config_factory = cls._get(name, cls.kind.MONITOR_CONFIG)
+        entity_factory = cls._get(name, cls.kind.MONITOR_ENTITY)
+        return (monitor_factory, config_factory, entity_factory)
+
+    @classmethod
+    def get_action_factories(cls, name):
+        action_factory = cls._get(name, cls.kind.ACTION)
+        config_factory = cls._get(name, cls.kind.ACTION_CONFIG)
+        entity_factory = cls._get(name, cls.kind.ACTION_ENTITY)
+        return (action_factory, config_factory, entity_factory)
+
+    @classmethod
+    def get_filter_factories(cls, name):
+        filter_factory = cls._get(name, cls.kind.FILTER)
+        return filter_factory
+
+    @classmethod
+    def register(cls, name: str, kind: kind):
+        def wrapper(func):
+            cls._register(name, kind, func)
+            return func
+        return wrapper
+
