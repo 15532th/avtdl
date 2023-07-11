24d9419601900a1d523bc67cc9e70c0a61a9826e
user <user@localhost>
2023-07-11 00:34:01 +0000

    Checkpoint

--- core/config.py
+++ core/config.py
@@ -1,4 +1,3 @@
-
 from dataclasses import dataclass
 from collections import defaultdict
 from enum import Enum
@@ -9,6 +8,7 @@ from typing import Dict, Tuple, List, Union, Callable
 
 from core.interfaces import Monitor, MonitorEntity, MonitorConfig, Action, ActionEntity, ActionConfig, Filter, Event
 from core.chain import Chain
+from core.plugins import Plugins
 
 class TopSectionName(Enum):
     monitors: str = 'Monitors'
@@ -92,72 +92,3 @@ class ConfigParser:
         chains = ConfigParser.parse_chains(filters, chains_section)
 
         return monitors, actions, filters, chains
-
-
-class Plugins:
-
-    class kind(Enum):
-        MONITOR = 'monitor'
-        MONITOR_CONFIG = 'monitor_config'
-        MONITOR_ENTITY = 'monitor_entity'
-        ACTION = 'action'
-        ACTION_CONFIG = 'action_config'
-        ACTION_ENTITY = 'action_entity'
-        FILTER = 'filter'
-
-    known: Dict[kind, Dict] = {k: {} for k in kind}
-
-    @classmethod
-    def _register(cls, name: str, kind: kind,
-                  instance: Union[Monitor, MonitorEntity, MonitorConfig,
-                                  Action, ActionEntity, ActionConfig,
-                                  Filter]):
-        cls.known[kind][name] = instance
-
-    @classmethod
-    def _get(cls, name: str, kind: kind):
-        instance = cls.known[kind].get(name)
-        if instance is None:
-            raise KeyError(f'"{name}" is not registered as "{kind}"')
-        return instance
-
-    @classmethod
-    def get_monitor_factories(cls, name):
-        monitor_factory = cls._get(name, cls.kind.MONITOR)
-        config_factory = cls._get(name, cls.kind.MONITOR_CONFIG)
-        entity_factory = cls._get(name, cls.kind.MONITOR_ENTITY)
-        return (monitor_factory, config_factory, entity_factory)
-
-    @classmethod
-    def get_action_factories(cls, name):
-        action_factory = cls._get(name, cls.kind.ACTION)
-        config_factory = cls._get(name, cls.kind.ACTION_CONFIG)
-        entity_factory = cls._get(name, cls.kind.ACTION_ENTITY)
-        return (action_factory, config_factory, entity_factory)
-
-    @classmethod
-    def get_filter_factory(cls, name):
-        filter_factory = cls._get(name, cls.kind.FILTER)
-        return filter_factory
-
-    @classmethod
-    def register(cls, name: str, kind: kind):
-        def wrapper(func):
-            cls._register(name, kind, func)
-            return func
-        return wrapper
-
-    @classmethod
-    def load(cls, directory='plugins'):
-        for item in Path(directory).glob('*'):
-            try:
-                module_name = '.'.join(item.parts)
-                m = importlib.import_module(module_name)
-                __import__(module_name, fromlist=m.__all__)
-            except Exception:
-                logging.exception(f'while trying to import {module_name}:')
-                continue
-            else:
-                logging.info('from {} imported {}'.format(module_name, ', '.join(m.__all__)))
-
-
