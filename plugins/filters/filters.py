88899dac1f8eca01080771fdfe86ea8536d00850
user <user@localhost>
2023-08-07 23:47:43 +0300

    Fixing run_command plugin, add event types and filter for events

--- plugins/filters/filters.py
+++ plugins/filters/filters.py
@@ -1,24 +1,25 @@
-from typing import List, Sequence
+from typing import List, Sequence, Optional
 
-from core.interfaces import Filter, Record, FilterEntity, ActorConfig
+from core.interfaces import Filter, Record, FilterEntity, ActorConfig, Event
 from core.config import Plugins
 
 @Plugins.register('filter.noop', Plugins.kind.ACTOR_CONFIG)
 @Plugins.register('filter.match', Plugins.kind.ACTOR_CONFIG)
 @Plugins.register('filter.exclude', Plugins.kind.ACTOR_CONFIG)
-class FilterConfig(ActorConfig):
+@Plugins.register('filter.event', Plugins.kind.ACTOR_CONFIG)
+class EmptyFilterConfig(ActorConfig):
     pass
 @Plugins.register('filter.noop', Plugins.kind.ACTOR_ENTITY)
-class NoopFilterEntity(FilterEntity):
+class EmptyFilterEntity(FilterEntity):
     name: str
 
 @Plugins.register('filter.noop', Plugins.kind.ACTOR)
 class NoopFilter(Filter):
 
-    def __init__(self, config: FilterConfig, entities: Sequence[NoopFilterEntity]):
+    def __init__(self, config: EmptyFilterConfig, entities: Sequence[EmptyFilterEntity]):
         super().__init__(config, entities)
 
-    def match(self, entity: str, record: Record):
+    def match(self, entity: str, record: Record) -> Optional[Record]:
         return record
 
 @Plugins.register('filter.match', Plugins.kind.ACTOR_ENTITY)
@@ -30,10 +31,10 @@ class MatchFilterEntity(FilterEntity):
 @Plugins.register('filter.match', Plugins.kind.ACTOR)
 class MatchFilter(Filter):
 
-    def __init__(self, config: FilterConfig, entities: Sequence[MatchFilterEntity]):
+    def __init__(self, config: EmptyFilterConfig, entities: Sequence[MatchFilterEntity]):
         super().__init__(config, entities)
 
-    def match(self, entity: str, record: Record):
+    def match(self, entity: str, record: Record) -> Optional[Record]:
         for pattern in self.entities[entity].patterns:
             if str(record).find(pattern) > -1:
                 return record
@@ -42,11 +43,32 @@ class MatchFilter(Filter):
 @Plugins.register('filter.exclude', Plugins.kind.ACTOR)
 class ExcludeFilter(Filter):
 
-    def __init__(self, config: FilterConfig, entities: Sequence[MatchFilterEntity]):
+    def __init__(self, config: EmptyFilterConfig, entities: Sequence[MatchFilterEntity]):
         super().__init__(config, entities)
 
-    def match(self, entity: str, record: Record):
+    def match(self, entity: str, record: Record) -> Optional[Record]:
         for pattern in self.entities[entity].patterns:
             if str(record).find(pattern) > -1:
                 return None
         return record
+
+@Plugins.register('filter.event', Plugins.kind.ACTOR_ENTITY)
+class EventFilterEntity(FilterEntity):
+    filter_type: List[str] = ['any']
+
+@Plugins.register('filter.event', Plugins.kind.ACTOR)
+class EventFilter(Filter):
+
+    def __init__(self, config: EmptyFilterConfig, entities: Sequence[EmptyFilterEntity]):
+        super().__init__(config, entities)
+
+    def match(self, entity: str, record: Record) -> Optional[Record]:
+        if isinstance(record, Event):
+            if record.filter_type == 'any':
+                return record
+            for filter_type in self.entities[entity].filter_type:
+                if record.filter_type == filter_type:
+                    return record
+        else:
+            return None
+
