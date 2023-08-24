4155aab3afd05e19de29bcb0c7337777c87c24a8
user <user@localhost>
2023-08-24 07:31:37 +0300

    Add plugin for monitoring fc2

---
+++ plugins/fc2/fc2.py
@@ -0,0 +1,77 @@
+import json
+import logging
+from json import JSONDecodeError
+from typing import Sequence, Optional
+
+import aiohttp
+import pydantic
+from pydantic import PrivateAttr
+
+from core.config import Plugins
+from core.interfaces import ActorConfig, Record, HttpTaskMonitorEntity, HttpTaskMonitor
+
+
+class FC2Record(Record):
+    user_id: str
+    start: str
+
+@Plugins.register('fc2', Plugins.kind.ACTOR_ENTITY)
+class FC2MonitorEntity(HttpTaskMonitorEntity):
+    user_id: str
+    update_interval: int = 300
+    latest_live_start: PrivateAttr = None
+
+@Plugins.register('fc2', Plugins.kind.ACTOR_CONFIG)
+class FC2MonitorConfig(ActorConfig):
+    pass
+
+@Plugins.register('fc2', Plugins.kind.ACTOR)
+class FC2Monitor(HttpTaskMonitor):
+
+    async def get_new_records(self, entity: FC2MonitorEntity, session: aiohttp.ClientSession) -> Sequence[FC2Record]:
+        record = await self.check_channel(entity, session)
+        return [record] if record else []
+
+    @classmethod
+    async def check_channel(cls, entity: FC2MonitorEntity, session: aiohttp.ClientSession) -> Optional[FC2Record]:
+        try:
+            data = await cls.get_metadata(entity, session)
+        except Exception as e:
+            logging.exception(f'FC2Monitor for {entity.name}: failed to check if channel {entity.user_id} is live: {e}')
+            return None
+        try:
+            record = cls.parse_metadata(data)
+        except (KeyError, TypeError, JSONDecodeError, pydantic.ValidationError) as e:
+            logging.exception(f'FC2Monitor for {entity.name}: failed to parse channel info. Raw response: {data}')
+            return None
+        if record.start == entity.latest_live_start:
+            logging.debug(f'FC2Monitor for {entity.name}: user {entity.user_id} is live since {entity.latest_live_start}, but record was already created')
+            return None
+        entity.latest_live_start = record.start
+        return record
+
+    @staticmethod
+    async def get_metadata(entity: FC2MonitorEntity, session: aiohttp.ClientSession):
+        url = 'https://live.fc2.com/api/memberApi.php'
+        params = {'channel': 1, 'streamid': entity.user_id}
+        async with session.post(url, data=params) as r:
+            data = await r.text()
+            return data
+
+    @staticmethod
+    def parse_metadata(data: str) -> Optional[FC2Record]:
+        data = json.loads(data)
+        data = data['data']['channel_data']
+        is_live = data['is_publish']
+        if not is_live:
+            return None
+        start = str(data['start'])
+        title = data['title']
+
+        channel_id = str(data['channelid'])
+        channel_url = f'https://live.fc2.com/{channel_id}/'
+
+        return FC2Record(url=channel_url, title=title, user_id=channel_id, start=start)
+
+
+
