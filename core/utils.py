7937e16a4e71de04a476a3405f2ea5fcb729a57a
user <user@localhost>
2023-08-28 07:49:46 +0300

    WIP on refactoring youtube rss

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,12 @@
+import datetime
+from email.utils import parsedate_tz
 import logging
+import re
 from http import cookiejar
 from pathlib import Path
 from typing import Optional
+
+import multidict
 
 
 def load_cookies(path: Optional[Path], raise_on_error: bool = False) -> Optional[cookiejar.CookieJar]:
@@ -21,3 +26,43 @@ def load_cookies(path: Optional[Path], raise_on_error: bool = False) -> Optional
         logging.exception(f'Failed to load cookies from {path}: {e}')
         return None
     return cookie_jar
+
+def get_cache_ttl(headers: multidict.CIMultiDictProxy) -> Optional[int]:
+    '''Check for Expires and Cache-Control headers,
+    return integer representing how many seconds has
+    left until resource is outdated'''
+
+    def get_expires_from_cache_control(headers) -> Optional[datetime.datetime]:
+        try:
+            cache_control = headers.get('Cache-control')
+            max_age = re.search('max-age=(\d+)', cache_control)
+            max_age_value = datetime.timedelta(seconds=int(max_age))
+
+            last_modified = headers.get('Last-Modified')
+            last_modified_value = datetime.datetime(*parsedate_tz(last_modified))
+
+            expires  = last_modified_value + max_age_value
+            return expires
+        except (TypeError, ValueError):
+            return None
+
+    def get_expires_from_expires_header(headers) -> Optional[datetime.datetime]:
+        try:
+            expires_header = headers.get('Expires')
+            if expires_header == '0':
+                return None
+            expires_value = datetime.datetime(*parsedate_tz(expires_header))
+            return expires_value
+        except (TypeError, ValueError):
+            return None
+
+    expires = get_expires_from_cache_control(headers) or get_expires_from_expires_header(headers)
+    if expires is None:
+        return None
+
+    now = datetime.datetime.utcnow()
+    delta = (expires - now).total_seconds()
+    if delta < 0:
+        return None
+
+    return int(delta)
