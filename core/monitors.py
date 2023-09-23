c8181647e811a2e3662f6f76f472d7beaf343b66
user <user@localhost>
2023-09-23 16:21:34 +0300

    Add BaseFeedMonitor, monitor class for downloading and parsing html pages with some kind of updating entities

--- core/monitors.py
+++ core/monitors.py
@@ -1,13 +1,18 @@
 import asyncio
+from datetime import datetime
+import json
+import sqlite3
 from abc import abstractmethod
 from collections import defaultdict
-from typing import Sequence, Dict, Optional
+import logging
+from typing import Sequence, Dict, Optional, Any
 
 import aiohttp
-from pydantic import FilePath
+from pydantic import FilePath, PrivateAttr
 
 from core import utils
 from core.interfaces import ActorEntity, Actor, ActorConfig, Record
+from core.utils import get_cache_ttl
 
 
 class TaskMonitorEntity(ActorEntity):
@@ -119,3 +124,140 @@ class HttpTaskMonitor(BaseTaskMonitor):
     @abstractmethod
     async def get_new_records(self, entity: TaskMonitorEntity, session: aiohttp.ClientSession) -> Sequence[Record]:
         '''Produce new records, optionally adjust update_interval'''
+
+
+class BaseFeedMonitorConfig(ActorConfig):
+    db_path: str = ':memory:'
+
+class BaseFeedMonitorEntity(HttpTaskMonitorEntity):
+    url: str
+    adjust_update_interval: bool = True
+    base_update_interval: PrivateAttr = None
+
+    def model_post_init(self, __context: Any) -> None:
+        self.base_update_interval = self.update_interval
+
+class BaseFeedMonitor(HttpTaskMonitor):
+
+    def __init__(self, conf: BaseFeedMonitorConfig, entities: Sequence[BaseFeedMonitorEntity]):
+        super().__init__(conf, entities)
+        self.db = RecordDB(conf.db_path, logger=self.logger.getChild('db'))
+
+    async def retrieve_records(self, entity: BaseFeedMonitorEntity, session: aiohttp.ClientSession, method='GET') -> Optional[str]:
+        '''Make http request to get raw data
+        Implementation might call this method or implement it itself'''
+        try:
+            async with session.request(method, entity.url) as response:
+                response.raise_for_status()
+                text = await response.text()
+        except Exception as e:
+            if isinstance(e, aiohttp.ClientResponseError):
+                self.logger.warning(f'[{entity.name}] got code {e.status} ({e.message}) while fetching {entity.url}')
+            else:
+                self.logger.warning(f'[{entity.name}] error while fetching {entity.url}: {e}')
+
+            update_interval = min(entity.update_interval * 2, entity.base_update_interval * 10, 4*3600)
+            if entity.update_interval != update_interval:
+                entity.update_interval = update_interval
+                self.logger.warning(f'update interval set to {entity.update_interval} seconds for {entity.name} ({entity.url})')
+            return None
+
+        if entity.adjust_update_interval:
+            update_interval = get_cache_ttl(response.headers) or entity.base_update_interval
+            entity.update_interval = max(update_interval, entity.base_update_interval)
+            self.logger.debug(f'Youtube RSS for {entity.name}: next update in {entity.update_interval}')
+        else:
+            # restore update interval after backoff on failure
+            if entity.update_interval != entity.base_update_interval:
+                self.logger.info(f'restoring update interval {entity.update_interval} seconds for {entity.name} ({entity.url})')
+                entity.update_interval = entity.base_update_interval
+
+        return text
+
+    @abstractmethod
+    async def get_records(self, entity: BaseFeedMonitorEntity, session: aiohttp.ClientSession) -> Sequence[Record]:
+        '''Fetch and parse resource, return parsed records, both old and new'''
+
+    @abstractmethod
+    def get_record_id(self, record: Record) -> str:
+        '''A string that unique identifies a record even if it has changed'''
+
+    async def prime_db(self, entity: BaseFeedMonitorEntity, session: aiohttp.ClientSession) -> None:
+        '''if feed has no prior records fetch it once and mark all entries as old
+        in order to not produce ten messages at once when feed first added'''
+        if self.db.get_size(entity.name) == 0:
+            self.logger.debug(f'[{entity.name}] database at "{self.conf.db_path}" has no records for "{entity.name}", assuming first run')
+            await self.get_new_records(entity, session)
+
+    def store_record(self, record: Record, entity: BaseFeedMonitorEntity):
+        uid = self.get_record_id(record)
+        parsed_at = datetime.utcnow()
+        hashsum = record.hash()
+        feed_name = entity.name
+        class_name = record.__class__.__name__
+        as_json = json.dumps(record.model_dump(), sort_keys=True, ensure_ascii=False, default=str)
+        self.db.store(parsed_at, feed_name, uid, hashsum, class_name, as_json)
+
+    def record_is_new(self, record: Record, entity: BaseFeedMonitorEntity) -> bool:
+        uid = self.get_record_id(record)
+        record_hash = record.hash()
+        if not self.db.row_exists(uid, record_hash):
+            self.store_record(record, entity)
+        exists = self.db.row_exists(uid)
+        return not exists
+
+    def filter_new_records(self, records: Sequence[Record], entity: BaseFeedMonitorEntity) -> Sequence[Record]:
+        new_records = []
+        for record in records:
+            if self.record_is_new(record, entity):
+                new_records.append(record)
+        return new_records
+
+    async def get_new_records(self, entity: BaseFeedMonitorEntity, session: aiohttp.ClientSession) -> Sequence[Record]:
+        records = await self.get_records(entity, session)
+        new_records = self.filter_new_records(records, entity)
+        return new_records
+
+class RecordDB:
+
+    def __init__(self, db_path, logger: Optional[logging.Logger] = None):
+        self.logger = logger or logging.getLogger('RecordDB')
+        try:
+            self.db = sqlite3.connect(db_path)
+            self.db.row_factory = sqlite3.Row
+            self.cursor = self.db.cursor()
+            record_structure = 'parsed_at datetime, feed_name text, uid text, hashsum text, class_name text, as_json text, PRIMARY KEY(uid, hashsum)'
+            self.cursor.execute('CREATE TABLE IF NOT EXISTS records ({})'.format(record_structure))
+            self.db.commit()
+        except sqlite3.OperationalError as e:
+            self.logger.error(
+                f'error opening sqlite database at path "{db_path}", specified in "db_path" config variable: {e}. If file exists make sure it was produced by this application, otherwise check if new file can be created at specified location. Alternatively use special value ":memory:" to use in-memory database instead.')
+            raise
+        else:
+            self.logger.debug(f'successfully connected to sqlite database at "{db_path}"')
+
+    def store(self, parsed_at: datetime, feed_name: str, uid: str, hashsum: str, class_name: str, as_json: str) -> None:
+        sql = 'INSERT INTO records VALUES(:parsed_at, :feed_name, :uid, :hashsum, :class_name, :as_json)'
+        row = {'parsed_at': parsed_at, 'feed_name': feed_name, 'uid': uid, 'hashsum': hashsum, 'class_name': class_name, 'as_json': as_json}
+        self.cursor.execute(sql, row)
+        self.db.commit()
+
+    def row_exists(self, uid: str, hashsum: Optional[str] = None) -> bool:
+        if hashsum is not None:
+            sql = "SELECT 1 FROM records WHERE uid=:uid AND hashsum=:hashsum LIMIT 1"
+        else:
+            sql = "SELECT 1 FROM records WHERE uid=:uid LIMIT 1"
+        keys = {'uid': uid, 'hashsum': hashsum}
+        self.cursor.execute(sql, keys)
+        return bool(self.cursor.fetchone())
+
+    def get_size(self, feed_name: Optional[str] = None) -> int:
+        '''return number of records, total or for specified feed, are stored in db'''
+        if feed_name is None:
+            sql = 'SELECT COUNT(1) FROM records'
+        else:
+            sql = 'SELECT COUNT(1) FROM records WHERE feed_name=:feed_name'
+        keys = {'feed_name': feed_name}
+        self.cursor.execute(sql, keys)
+        return int(self.cursor.fetchone()[0])
+
