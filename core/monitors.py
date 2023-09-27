8fff2269081df43288f32a4f9822b712b34f51a7
user <user@localhost>
2023-09-27 18:37:26 +0300

    In BaseFeedMonitor.retrieve_records only log adjusted update inverval if it has changed

--- core/monitors.py
+++ core/monitors.py
@@ -164,8 +164,10 @@ class BaseFeedMonitor(HttpTaskMonitor):
 
         if entity.adjust_update_interval:
             update_interval = get_cache_ttl(response.headers) or entity.base_update_interval
-            entity.update_interval = max(update_interval, entity.base_update_interval)
-            self.logger.debug(f'Youtube RSS for {entity.name}: next update in {entity.update_interval}')
+            new_update_interval = max(update_interval, entity.base_update_interval)
+            if entity.update_interval != new_update_interval:
+                self.logger.debug(f'[{entity.name}] next update in {entity.update_interval}')
+                entity.update_interval = new_update_interval
         else:
             # restore update interval after backoff on failure
             if entity.update_interval != entity.base_update_interval:
