d06f3a9d372316cd781b513a3ec6803c8e7bf7c7
user <user@localhost>
2023-09-27 18:24:28 +0300

    In UrlMonitor, wrap get_new_records in try-except

--- plugins/url/url.py
+++ plugins/url/url.py
@@ -22,8 +22,12 @@ class UrlMonitorEntity(TaskMonitorEntity):
 @Plugins.register('get_url', Plugins.kind.ACTOR)
 class UrlMonitor(TaskMonitor):
     async def get_new_records(self, entity: UrlMonitorEntity):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(entity.url) as r:
-                text = await r.text()
-                record = TextRecord(text=text)
-        return [record]
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(entity.url) as r:
+                    text = await r.text()
+                    record = TextRecord(text=text)
+            return [record]
+        except Exception as e:
+            self.logger.warning(f'[{entity.name}] failed to fetch url {entity.url}: {e}')
+            return []
