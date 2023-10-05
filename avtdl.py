38dccfb1aa802cb2b090bd6660c630b183ca77f0
user <user@localhost>
2023-10-05 14:39:53 +0300

    Reimplement validation of actors and entities names used in chains

--- avtdl.py
+++ avtdl.py
@@ -55,18 +55,15 @@ def main(config_path: Path):
         logging.exception(e)
         raise SystemExit from e
 
-    for name, actor_instance in actors.items():
-        subscribed = actor_instance.bus.get_subscribed().get(name, [])
-        exist = actor_instance.entities.keys()
-        orphans = set(subscribed).difference(set(exist))
-        for orphan in orphans:
-            for chain_name, chain_instance in chains.items():
-                for actor_name, entities in chain_instance.conf:
-                    if actor_name != name:
-                        continue
-                    if orphan in entities:
-                        logging.warning(f'chain "{chain_name}" references "{actor_name}: {orphan}", but actor "{actor_name}" has no "{orphan}" entity. It might be a typo in the chain configuration')
-
+    for chain_name, chain_instance in chains.items():
+        for actor_name, entities in chain_instance.conf:
+            actor = actors.get(actor_name)
+            if actor is None:
+                logging.warning(f'chain "{chain_name}" references actor "{actor_name}, absent in "Actors" section. It might be a typo in the chain configuration')
+                continue
+            orphans = set(entities) - actor.entities.keys()
+            for orphan in orphans:
+                logging.warning(f'chain "{chain_name}" references "{actor_name}: {orphan}", but actor "{actor_name}" has no "{orphan}" entity. It might be a typo in the chain conf  iguration')
 
     asyncio.run(run(actors.values()), debug=True)
 
