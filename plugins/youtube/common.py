989876ac39750b7b08730ed4f7bbc6f0b6fcc28e
user <user@localhost>
2023-11-18 19:30:32 +0300

    Trying to speed youtube page parser up:
    replace jsonpath library, adapt jsonpath strings as they are subtle different
    make some jsonpath expressions to use less '..' in hope to speed it up
    in get_video_renderers() add shortcut jsonpath expression that should work fast but only matches channel videos/streams tab
    rewrite get_initial_data() to rely on fast regexp with fallback to counting parentheses in case data contain semicolon
    rewrite get_initial_player_response() as copypaste of get_initial_data()

--- plugins/youtube/common.py
+++ plugins/youtube/common.py
@@ -1,15 +1,16 @@
 import json
 import re
+from collections import defaultdict
 from typing import Any, Optional
 
-from jsonpath_ng import ext as jsonpath_ng
+from jsonpath import JSONPath
 
 
 def find_all(data: Any, jsonpath: str, cache={}) -> list:
     if jsonpath not in cache:
-        cache[jsonpath] = jsonpath_ng.parse(jsonpath)
+        cache[jsonpath] = JSONPath(jsonpath)
     parser = cache[jsonpath]
-    return [item.value for item in parser.find(data)]
+    return parser.parse(data)
 
 
 def find_one(data: Any, jsonpath: str) -> Optional[Any]:
@@ -18,7 +19,14 @@ def find_one(data: Any, jsonpath: str) -> Optional[Any]:
 
 
 def get_initial_data(page: str) -> dict:
-    re_initial_data = 'var ytInitialData = ({.*?});'
+    try:
+        return get_initial_data_fast(page)
+    except ValueError:
+        return get_initial_data_slow(page)
+
+@profile
+def get_initial_data_fast(page: str) -> dict:
+    re_initial_data = 'var ytInitialData = ([^;]*);'
     match = re.search(re_initial_data, page)
     if match is None:
         raise ValueError(f'Failed to find ytInitialData on the page')
@@ -26,6 +34,29 @@ def get_initial_data(page: str) -> dict:
     data = json.loads(raw_data)
     return data
 
+@profile
+def get_initial_data_slow(page: str) -> dict:
+    anchor = 'var ytInitialData = {'
+    pos_start = page.find(anchor)
+    if pos_start == -1:
+        raise ValueError(f'Failed to find initial data on page')
+    pos_start += len(anchor) - 1
+    position = pos_start
+
+    re_parenthesses = re.compile('[{}]')
+    parenthesses_values = defaultdict(int, {'{': 1, '}': -1})
+    parentheses = 0
+    while True:
+        parentheses += parenthesses_values[page[position]]
+        if parentheses == 0:
+            raw_data = page[pos_start:position+1]
+            response = json.loads(raw_data)
+            return response
+        position_match = re_parenthesses.search(page, position + 1)
+        try:
+            position = position_match.start()
+        except AttributeError:
+            raise ValueError(f'Failed to find matching set of parentheses after initial data')
 
 def thumbnail_url(video_id: str) -> str:
     return f'https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg'
