60c4c8241924204e4c3052d59d1a7f978056406d
user <user@localhost>
2023-12-30 07:43:46 +0300

    Add docstrings in twitcast_monitor.py  Set default update_interval from 500 to 60 seconds

--- plugins/twitcast/twitcast_monitor.py
+++ plugins/twitcast/twitcast_monitor.py
@@ -10,12 +10,16 @@ from core.monitors import HttpTaskMonitor, HttpTaskMonitorEntity
 
 
 class TwitcastRecord(Record):
-
     user_id: str
+    """unique part of channel url"""
     movie_id: str
+    """unique id for current livestream"""
     url: str
+    """user (channel) url"""
     movie_url: str
+    """current livestream url"""
     title: str
+    """livestream title"""
 
     def __str__(self):
         return f'{self.url}\n{self.title} ({self.movie_id})'
@@ -27,8 +31,12 @@ class TwitcastRecord(Record):
 @Plugins.register('twitcast', Plugins.kind.ACTOR_ENTITY)
 class TwitcastMonitorEntity(HttpTaskMonitorEntity):
     user_id: str
-    update_interval: int = 300
+    """user id that should be monitored"""
+    update_interval: int = 60
+    """how often user will be checked for being live, in seconds"""
     most_recent_movie: Optional[str] = Field(exclude=True, default=None)
+    """internal variable to persist state between updates. Used to keep movie_id to detect if current live is the same as in the last update"""
+
 
 @Plugins.register('twitcast', Plugins.kind.ACTOR_CONFIG)
 class TwitcastMonitorConfig(ActorConfig):
@@ -36,6 +44,17 @@ class TwitcastMonitorConfig(ActorConfig):
 
 @Plugins.register('twitcast', Plugins.kind.ACTOR)
 class TwitcastMonitor(HttpTaskMonitor):
+    """
+    Monitor for twitcasting.tv
+
+    Monitors twitcasting.tv user with given id, produces record when it goes live.
+    For user https://twitcasting.tv/c:username user id would be "c:username".
+
+    Rate limits for endpoint used to check if user is live are likely relatively high,
+    but it is better to keep "update_interval" big enough for combined amount of updates
+    for all monitored users to not exceed one request per second.
+    """
+
 
     async def get_new_records(self, entity: TwitcastMonitorEntity, session: aiohttp.ClientSession) -> Sequence[TwitcastRecord]:
         record = await self.check_channel(entity, session)
