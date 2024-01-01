a41a6ab96499ff09f93b904bb6bd223317a4a895
user <user@localhost>
2024-01-01 14:59:08 +0300

    Rewrite UrlMonitor to use HttpTaskMonitor facilities, add docstrings

--- plugins/url/url.py
+++ plugins/url/url.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
+from typing import Optional
 
 import aiohttp
+from pydantic import Field
 
 from core.config import Plugins
-from core.interfaces import ActorEntity, TextRecord
-from core.monitors import TaskMonitorEntity, TaskMonitor
+from core.interfaces import ActorConfig, TextRecord
+from core.monitors import HttpTaskMonitor, HttpTaskMonitorEntity
 
 
 @Plugins.register('get_url', Plugins.kind.ACTOR_CONFIG)
 @dataclass
-class UrlMonitorConfig(ActorEntity):
+class UrlMonitorConfig(ActorConfig):
     pass
 
 @Plugins.register('get_url', Plugins.kind.ACTOR_ENTITY)
 @dataclass
-class UrlMonitorEntity(TaskMonitorEntity):
+class UrlMonitorEntity(HttpTaskMonitorEntity):
     url: str
+    """url to monitor"""
+    last_record_hash: Optional[str] = Field(exclude=True, default=None)
+    """internal variable to persist state between updates. Used to keep hash of page content on previous update"""
 
 @Plugins.register('get_url', Plugins.kind.ACTOR)
-class UrlMonitor(TaskMonitor):
-    async def get_new_records(self, entity: UrlMonitorEntity):
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(entity.url) as r:
-                    text = await r.text()
-                    record = TextRecord(text=text)
-            return [record]
-        except Exception as e:
-            self.logger.warning(f'[{entity.name}] failed to fetch url {entity.url}: {e}')
+class UrlMonitor(HttpTaskMonitor):
+    """
+    Monitor url content
+
+    Download web-page content and emit it as a text record if it has changed.
+    """
+
+    async def get_new_records(self, entity: UrlMonitorEntity, session: aiohttp.ClientSession):
+        text = await self.request(entity.url, entity, session)
+        if text is None:
             return []
+        record = TextRecord(text=text)
+        record_hash = record.hash()
+        if record_hash != entity.last_record_hash:
+            entity.last_record_hash = record_hash
+            return [record]
+        return []
