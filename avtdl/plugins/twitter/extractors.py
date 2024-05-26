2be3eb3716560d6aba985e11d5987a3e66dbfc8b
user <user@localhost>
2024-05-26 04:39:38 +0300

    WIP on twitter records parsers

--- avtdl/plugins/twitter/extractors.py
+++ avtdl/plugins/twitter/extractors.py
@@ -1,6 +1,67 @@
+import datetime
+import json
+from pathlib import Path
+from textwrap import shorten
+from time import perf_counter_ns
+from typing import List, Optional, Tuple
+
+import dateutil.parser
+from line_profiler_pycharm import profile
 from pydantic import BaseModel
 
-from avtdl.core.utils import find_one
+from avtdl.core.interfaces import MAX_REPR_LEN, Record
+from avtdl.core.utils import find_all, find_one
+from avtdl.plugins.youtube.common import extract_keys
+
+
+class TwitterRecord(Record):
+    """
+    Single post as parsed from Twitter instance
+
+    Depending on the tweet type (regular, retweet, reply, quote) some fields might be empty
+    """
+    url: str
+    """tweet url"""
+    author: str
+    """user's visible name"""
+    username: str
+    """user's handle"""
+    avatar_url: Optional[str] = None
+    """link to the picture used as the user's avatar"""
+    published: datetime.datetime
+    """tweet timestamp"""
+    text: str
+    """tweet text with stripped formatting"""
+    attachments: List[str] = []
+    """list of links to attached images or video thumbnails"""
+    replying_to_username: Optional[str] = None
+    """for replies, name of the user that got replied to"""
+    retweet: Optional['TwitterRecord'] = None
+    """Nested TwitterRecord containing tweet that was retweeted"""
+    quote: Optional['TwitterRecord'] = None
+    """Nested TwitterRecord containing tweet being quited"""
+
+    def __repr__(self):
+        return f'TwitterRecord(author="{self.author}", url="{self.url}", text="{shorten(self.text, MAX_REPR_LEN)}")'
+
+    def __str__(self):
+        tweet = self.retweet or self
+        elements = []
+        elements.append(tweet.url)
+        if self.retweet is not None:
+            retweet_header = f'[{self.published.strftime("%Y-%m-%d %H:%M:%S")}] {self.author} (@{self.username}) has retweeted:'
+            elements.append(retweet_header)
+        if tweet.replying_to_username is not None:
+            reply_header = f'Replying to @{tweet.replying_to_username}:'
+            elements.append(reply_header)
+        elements.append(f'[{tweet.published.strftime("%Y-%m-%d %H:%M:%S")}] {tweet.author} (@{tweet.username}):')
+        elements.append(tweet.text)
+        if tweet.attachments:
+            elements.append('\n'.join(tweet.attachments))
+        if tweet.quote:
+            elements.append('\nReferring to ')
+            elements.append(str(tweet.quote))
+        return '\n'.join(elements)
 
 
 class UserInfo(BaseModel):
@@ -9,7 +70,7 @@ class UserInfo(BaseModel):
     name: str
     description: str
     avatar_url: str
-    banner_url: str
+    banner_url: Optional[str] = None
     location: str
 
     @classmethod
@@ -20,6 +81,7 @@ class UserInfo(BaseModel):
         return cls.from_result(result)
 
     @classmethod
+    @profile
     def from_result(cls, result: dict) -> 'UserInfo':
         typename = result.get('__typename')
         if typename != 'User':
@@ -31,7 +93,204 @@ class UserInfo(BaseModel):
         handle = legacy['screen_name']
         name = legacy['name']
         description = legacy['description']
-        avatar_url = legacy['profile_image_url_https'].replace('_normal', '_400x400')
-        banner_url = legacy['profile_banner_url']
+        avatar_url = legacy['profile_image_url_https'].replace('_normal', '')
+        banner_url = legacy.get('profile_banner_url')
         location = legacy['location']
         return cls(rest_id=rest_id, handle=handle, name=name, description=description, avatar_url=avatar_url, banner_url=banner_url, location=location)
+
+
+@profile
+def extract_contents(data: str) -> Tuple[List[dict], Optional[str]]:
+    """Picks all tweets, individual and inside conversations. Also picks bottom cursor value"""
+    tweets = []
+    continuation = None
+
+    def handle_item(obj):
+        nonlocal continuation
+        if '__typename' not in obj:
+            return obj
+        # tweet
+        if 'tweet_results' in obj:
+            tweets.append(obj['tweet_results'])
+            return obj
+        # cursor
+        typename = obj['__typename']
+        if typename == 'TimelineTimelineCursor':
+            if obj.get('cursorType') == 'Bottom':
+                continuation = obj.get('value')
+            return obj
+        return obj
+
+    decoder = json.JSONDecoder(object_hook=handle_item)
+    data, _ = decoder.raw_decode(data)
+    return tweets, continuation
+
+
+def get_continuation(contents: List[dict]) -> Optional[str]:
+    for item in reversed(contents):  # cursor is likely going to be last on the list
+        if item.get('__typename') == 'TimelineTimelineCursor':
+            if item.get('cursorType') == 'Bottom':
+                return item.get('value')
+    return None
+
+
+@profile
+def parse_tweet(tweet_results: dict):
+    tweet_result = tweet_results.get('result')
+    if tweet_result is None:
+        raise ValueError(f'failed to parse tweet: no "result"')
+    typename = tweet_result.get('__typename')
+    if typename == 'TweetWithVisibilityResults':
+        tweet_result = tweet_result['tweet']
+    elif typename != 'Tweet':
+        raise ValueError(f'failed to parse tweet: __typename is "{typename}, expected "Tweet"')
+    rest_id = tweet_result['rest_id']
+
+    # user_result = find_one(tweet_result, '$.core.user_results.result')
+    try:
+        user_result = tweet_result['core']['user_results']['result']
+    except (KeyError, TypeError):
+        raise ValueError(f'failed to parse tweet: no user_result found. Raw tweet_result: "{tweet_result}"')
+    try:
+        user = UserInfo.from_result(user_result)
+    except Exception as e:
+        raise
+    url = f'https://twitter.com/{user.handle}/status/{rest_id}'
+
+    legacy = tweet_result['legacy']
+    published = dateutil.parser.parse(legacy['created_at'])
+
+    try:
+        text = tweet_text(tweet_result)
+    except Exception as e:
+        raise
+
+    attachments = parse_media(tweet_result) or []
+
+    replying_to_username: Optional[str] = legacy.get('in_reply_to_screen_name')
+
+    retweet_result = legacy.get('retweeted_status_result')
+    retweet = parse_tweet(retweet_result) if retweet_result else None
+    if retweet is not None:
+        text = retweet.text
+
+    quote_tesult = tweet_result.get('quoted_status_result')
+    quote = parse_tweet(quote_tesult) if quote_tesult else None
+
+    tweet = TwitterRecord(
+        url=url,
+        author=user.name,
+        username=user.handle,
+        avatar_url=user.avatar_url,
+        published=published,
+        text=text,
+        attachments=attachments,
+        replying_to_username=replying_to_username,
+        retweet=retweet,
+        quote=quote
+    )
+    return tweet
+
+
+@profile
+def parse_media(tweet_result: dict) -> List[str]:
+    try:
+        legacy = tweet_result['legacy']
+    except KeyError as e:
+        raise ValueError(f'failed to parse tweet media: no {e} found')
+    media = (legacy.get('entities') or {}).get('media', [])
+    extended_media = (legacy.get('extended_entities') or {}).get('media', [])
+    if not media and not extended_media:
+        return []
+    attachments = [media_item['media_url_https'] for media_item in media]
+    images = []
+    videos = []
+    for item in media:
+        if item['type'] == 'photo':
+            media_url = item['media_url_https']
+            images.append(media_url)
+        elif item['type'] in ['video', 'animated_gif']:
+            try:
+                variants = item['video_info']['variants']
+            except (KeyError, TypeError):
+                variants = []
+            best_variant = max(variants, key=lambda variant: variant.get('bitrate', -1))
+            video_url = best_variant['url']
+            videos.append(video_url)
+        else:
+            ...  # unknown media type
+    return attachments
+
+
+@profile
+def tweet_text(tweet_result: dict) -> str:
+    try:
+        legacy = tweet_result['legacy']
+        text = legacy['full_text']
+        entities = legacy['entities']
+    except (KeyError, TypeError) as e:
+        raise ValueError(f'failed to parse tweet text: no {e} found')
+    urls = entities.get('urls', [])
+    for url in urls:
+        try:
+            text = text.replace(url['url'], url['expanded_url'])
+        except KeyError:
+            pass
+    img_urls = entities.get('media', [])
+    for url in img_urls:
+        try:
+            text = text.replace(url['url'], '\n' + url['expanded_url'])
+        except KeyError:
+            pass
+    return text
+
+
+@profile
+def main():
+    for name in Path('.').glob('*.json'):
+        tweets = []
+        print(f'*** {name} ***')
+        with open(name, 'rt', encoding='utf8') as fp:
+            text = fp.read()
+        extract_1(text)
+        extract_2(text)
+        t1 = perf_counter_ns()
+        raw_tweets, continuation = extract_contents(text)
+        t2 = perf_counter_ns()
+        for tweet_result in raw_tweets:
+            tweet = parse_tweet(tweet_result)
+            tweets.append(tweet)
+        t3 = perf_counter_ns()
+        print(f'extract: {(t2 - t1) / 10 ** 6}, parse: {(t3 - t2) / 10 ** 6}')
+        ...
+        with Path(f'D:/test/{name}.txt').open('wt', encoding='utf8') as fp:
+            for tweet in tweets:
+                fp.write(str(tweet.as_timezone()))
+                fp.write('\n\n' + '*' * 80 + '\n')
+    ...
+
+
+@profile
+def extract_1(text):
+    t1 = perf_counter_ns()
+    data = json.loads(text)
+    t2 = perf_counter_ns()
+    tweet_results = find_all(data, '$..tweet_results')
+    t3 = perf_counter_ns()
+    content = find_all(data, '$..content')
+    entries = find_one(data, '$..entries')
+    items = find_all(data, '$..content.itemContent')
+    non_items = [x for x in content if 'itemContent' not in x]
+    print(f'json: {(t2 - t1) / 10 ** 6}, find: {(t3 - t2) / 10 ** 6}')
+
+
+@profile
+def extract_2(text):
+    t1 = perf_counter_ns()
+    stuff, data = extract_keys(text, ['content', 'tweet_results'])
+    t2 = perf_counter_ns()
+    print(f'extract: {(t2 - t1) / 10 ** 6}')
+
+
+if __name__ == '__main__':
+    main()
