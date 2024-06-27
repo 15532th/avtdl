57e9967a98783dcafc1ee83dabcec5b76928cac1
user <user@localhost>
2024-06-27 03:18:44 +0300

    Refactor TwitterSpace.handle_space()

--- avtdl/plugins/twitter/spaces.py
+++ avtdl/plugins/twitter/spaces.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import json
 import urllib.parse
 from enum import Enum
 from typing import Optional, Sequence, Set
@@ -12,7 +13,7 @@ from avtdl.core.interfaces import Action, ActionEntity, ActorConfig, Record
 from avtdl.core.plugins import Plugins
 from avtdl.core.utils import SessionStorage, find_matching_field_value, monitor_tasks_set
 from avtdl.plugins.twitter.endpoints import AudioSpaceEndpoint, LiveStreamEndpoint
-from avtdl.plugins.twitter.extractors import TwitterSpaceRecord, find_space_id, parse_space, space_url_by_id
+from avtdl.plugins.twitter.extractors import TwitterSpaceRecord, find_space_id, parse_media_url, parse_space, space_url_by_id
 
 Plugins.register('twitter.space', Plugins.kind.ASSOCIATED_RECORD)(TwitterSpaceRecord)
 
@@ -69,32 +70,65 @@ class TwitterSpace(Action):
         await monitor_tasks_set(self.tasks)
 
     async def handle_space(self, entity: TwitterSpaceEntity, space_id: str):
+        should_emit_something: bool = entity.emit_upcoming
+        should_emit_live_url: bool = entity.emit_on_start
+        should_emit_replay_url: bool = entity.emit_on_end
+
+        def handle_update_result(space: TwitterSpaceRecord):
+            nonlocal should_emit_something
+            nonlocal should_emit_live_url
+            nonlocal should_emit_replay_url
+            if should_emit_replay_url:
+                if space.media_url is not None and StreamUrlType.is_replay(space.media_url):
+                    self.on_record(entity, space)
+                    should_emit_replay_url = False
+                    should_emit_live_url = False
+                    should_emit_something = False
+            if should_emit_live_url:
+                if space.media_url is not None and StreamUrlType.is_live(space.media_url):
+                    self.on_record(entity, space)
+                    should_emit_live_url = False
+                    should_emit_something = False
+            if should_emit_something:
+                self.on_record(entity, space)
+                should_emit_something = False
+
         session = self.sessions.get_session(entity.cookies_file)
         space = await self.fetch_space(session, entity, space_id)
         if space is None:
             return
-        if SpaceState.is_upcoming(space):
-            await self.handle_upcoming(session, entity, space)
-            if entity.emit_on_end:
-                await self.handle_ended(session, entity, space)
-            return
-        elif SpaceState.is_ongoing(space):
-            await self.handle_ongoing(session, entity, space)
-            return
-        elif SpaceState.has_ended(space):
-            await self.handle_ended(session, entity, space)
-            return
-        else:
-            self.logger.warning(f'[{entity.name}] space {space.url} state is unknown, aborting processing. {space.model_dump()}')
+        space.media_url = await self.fetch_media_url(session, entity, space)
+
+        handle_update_result(space)
+        if not (should_emit_something or should_emit_live_url or should_emit_replay_url):
             return
 
+        while True:
+            if SpaceState.is_upcoming(space):
+                media_url = await self.wait_for_live(session, entity, space)
+            elif SpaceState.is_ongoing(space):
+                media_url = await self.wait_for_archive(session, entity, space)
+            elif SpaceState.has_ended(space):
+                media_url = await self.fetch_media_url(session, entity, space)
+                if media_url is None:
+                    self.logger.warning(f'[{entity.name}] space {space.url} has ended at {space.ended} and media url is unavailable. The space likely does not have archive at this point')
+                    break
+            else:
+                self.logger.warning(f'[{entity.name}] space {space.url} state is unknown, aborting. {space.model_dump()}')
+                break
 
-    async def handle_upcoming(self, session: aiohttp.ClientSession, entity: TwitterSpaceEntity, space: TwitterSpaceRecord):
-        if entity.emit_upcoming:
-            self.on_record(entity, space)
+            if media_url is not None:
+                space.media_url = media_url
+
+            handle_update_result(space)
+            if not (should_emit_something or should_emit_live_url or should_emit_replay_url):
+                break
+
+            space = await self.fetch_space(session, entity, space_id) or space
+
+
+    async def wait_for_live(self, session: aiohttp.ClientSession, entity: TwitterSpaceEntity, space: TwitterSpaceRecord) -> Optional[str]:
         assert space.scheduled is not None, f'upcoming space has no scheduled: {space.model_dump()}'
-        if not entity.emit_on_start and not entity.emit_on_end:
-            return
         while True: # waiting until start
             delay = (datetime.datetime.now(tz=datetime.timezone.utc) - space.scheduled)
             delay_seconds = delay.total_seconds()
@@ -102,65 +136,34 @@ class TwitterSpace(Action):
                 break
             self.logger.debug(f'[{entity.name}] space {space.url} starts at {space.scheduled}, sleeping for {delay}')
             await asyncio.sleep(delay_seconds)
-            space = await self.fetch_space(session, entity, space.uid) or space
         self.logger.debug(f'[{entity.name}] space {space.url} should start now, fetching media_url')
+
         for attempt in range(300):
             media_url = await self.fetch_media_url(session, entity, space)
             if media_url is not None:
-                space.media_url = media_url
-                if entity.emit_on_start:
-                    self.on_record(entity, space)
-                break
+                return media_url
             self.logger.debug(f'[{entity.name}] upcoming space {space.url} got no media_url on {attempt} attempt, will try again')
             await asyncio.sleep(30)
-        else:
-            self.logger.debug(f'[{entity.name}] failed to fetch media_url for ongoing space {space.url}, giving up')
+        self.logger.debug(f'[{entity.name}] failed to fetch media_url for supposedly ongoing space {space.url}, giving up')
+        return None
 
-    async def handle_ongoing(self, session: aiohttp.ClientSession, entity: TwitterSpaceEntity, space: TwitterSpaceRecord):
+    async def wait_for_archive(self, session: aiohttp.ClientSession, entity: TwitterSpaceEntity, space: TwitterSpaceRecord):
         self.logger.debug(f'[{entity.name}] space {space.url} is ongoing since {space.started}, fetching live media_url')
         for attempt in range(100):
             media_url = await self.fetch_media_url(session, entity, space)
-            if media_url is not None:
-                space.media_url = media_url
-                if StreamUrlType.is_live(media_url):
-                    self.logger.debug(f'[{entity.name}] {space.url} got livestream media_url: "{media_url}"')
-                    if entity.emit_on_start:
-                        self.on_record(entity, space)
-                    if entity.emit_on_end:
-                        await self.handle_ended(session, entity, space)
-                elif StreamUrlType.is_replay(media_url):
-                    self.logger.debug(f'[{entity.name}] {space.url} got replay media_url: "{media_url}"')
-                    if entity.emit_on_end:
-                        self.on_record(entity, space)
-                else:
-                    self.logger.warning(f'[{entity.name}] {space.url} got media_url with unknown type: "{media_url}"')
-                break
-            self.logger.debug(f'[{entity.name}] supposedly ongoing space {space.url} got no media_url on {attempt} attempt, will try again')
+            if media_url is None:
+                self.logger.debug(f'[{entity.name}] supposedly ongoing space {space.url} got no media_url on {attempt} attempt, will try again')
+            elif StreamUrlType.is_live(media_url):
+                pass
+            elif StreamUrlType.is_replay(media_url):
+                return media_url
+            else:
+                self.logger.debug(f'[{entity.name}] space {space.url} got media_url with unknown type: {media_url}')
+                return media_url
+
             await asyncio.sleep(300)
-        else:
-            self.logger.debug(f'[{entity.name}] failed to fetch media_url for ongoing space {space.url}, giving up')
-
-
-    async def handle_ended(self, session: aiohttp.ClientSession, entity: TwitterSpaceEntity, space: TwitterSpaceRecord):
-        self.logger.debug(f'[{entity.name}] space {space.url} has ended at {space.ended}, fetching replay media_url')
-        for attempt in range(10):
-            media_url = await self.fetch_media_url(session, entity, space)
-            if media_url is not None:
-                space.media_url = media_url
-                if StreamUrlType.is_live(media_url):
-                    self.logger.debug(f'[{entity.name}] {space.url} on {attempt} attempt got livestream media_url, will try again later. media_url: "{media_url}"')
-                elif StreamUrlType.is_replay(media_url):
-                    self.logger.debug(f'[{entity.name}] {space.url} on {attempt} attempt got replay media_url, done. media_url: "{media_url}"')
-                    self.on_record(entity, space)
-                    break
-                else:
-                    self.logger.warning(f'[{entity.name}] {space.url} on {attempt} attempt got media_url with unexpected type: "{media_url}"')
-                    break
-            self.logger.debug(f'[{entity.name}] on {attempt} attempt failed to fetch media_url for ended space {space.url}, will try again')
-            await asyncio.sleep(300)
-        else:
-            self.logger.debug(f'[{entity.name}] failed to fetch media_url for ended space {space.url}, giving up')
-
+        self.logger.debug(f'[{entity.name}] failed to fetch media_url for ongoing space {space.url}, giving up')
+        return None
 
     async def fetch_space(self, session: aiohttp.ClientSession, entity: TwitterSpaceEntity, space_id: str) -> Optional[TwitterSpaceRecord]:
         self.logger.debug(f'[{entity.name}] fetch space {space_id}')
