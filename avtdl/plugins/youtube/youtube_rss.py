e355f116e979e618348b8611a1ac2e622fed8f28
user <user@localhost>
2024-07-09 07:11:05 +0300

    In youtube_rss.py change FeedMonitor to use generic RecordDB used by BaseFeedMonitor
    Add code for converting existing database files into new format on load

--- avtdl/plugins/youtube/youtube_rss.py
+++ avtdl/plugins/youtube/youtube_rss.py
@@ -1,7 +1,9 @@
 import logging
+import re
 import sqlite3
 from datetime import datetime, timezone
-from typing import Any, Dict, List, Optional, Sequence, Union
+from pathlib import Path
+from typing import Any, Dict, Iterator, List, Optional, Sequence, Union
 
 import aiohttp
 import feedparser
@@ -9,7 +11,6 @@ from pydantic import ConfigDict
 
 from avtdl.core import utils
 from avtdl.core.config import Plugins
-from avtdl.core.db import BaseRecordDB
 from avtdl.core.interfaces import Record
 from avtdl.plugins.rss.generic_rss import GenericRSSMonitor, GenericRSSMonitorConfig, GenericRSSMonitorEntity
 from avtdl.plugins.youtube.common import thumbnail_url
@@ -105,38 +106,18 @@ class YoutubeFeedRecord(Record):
         return embed
 
 
-class RecordDB(BaseRecordDB):
-    table_structure = 'parsed_at datetime, feed_name text, author text, video_id text, link text, title text, summary text, published datetime, updated datetime, scheduled datetime DEFAULT NULL, views integer, PRIMARY KEY(video_id, updated)'
-    row_structure = ':parsed_at, :feed_name, :author, :video_id, :url, :title, :summary, :published, :updated, :scheduled, :views'
-    id_field = 'video_id'
-    exact_id_field = 'updated'
-    group_id_field = 'feed_name'
-    sorting_field = 'parsed_at'
-
-    def store(self, rows: Union[Dict[str, Any], List[Dict[str, Any]]]) -> None:
-        return super().store(rows)
-
-    def row_exists(self, video_id: str, updated: Optional[datetime] = None) -> bool:
-        return super().row_exists(video_id, updated)
-
-    def fetch_row(self, video_id: str, updated: Optional[datetime] = None) -> Optional[sqlite3.Row]:
-        return super().fetch_row(video_id, updated)
-
-    def get_size(self, feed_name: Optional[str] = None) -> int:
-        '''return number of records, total or for specified feed, are stored in db'''
-        return super().get_size(feed_name)
-
-
 @Plugins.register('rss', Plugins.kind.ACTOR_ENTITY)
 class FeedMonitorEntity(GenericRSSMonitorEntity):
-    update_interval : float = 900
+    update_interval: float = 900
     """How often the feed should be updated, in seconds"""
     track_reschedule: bool = False
     """keep track of scheduled time of upcoming streams, emit record again if it is changed to an earlier date"""
 
+
 @Plugins.register('rss', Plugins.kind.ACTOR_CONFIG)
 class FeedMonitorConfig(GenericRSSMonitorConfig):
     pass
+
 
 @Plugins.register('rss', Plugins.kind.ACTOR)
 class FeedMonitor(GenericRSSMonitor):
@@ -168,9 +149,11 @@ class FeedMonitor(GenericRSSMonitor):
     `adjust_update_interval` to disable this behavior.
     """
 
-    RecordDB = RecordDB
-
     def __init__(self, conf: FeedMonitorConfig, entities: Sequence[FeedMonitorEntity]):
+        try:
+            Migration(conf.db_path)
+        except Exception as e:
+            raise Exception('migration failed') from e
         super().__init__(conf, entities)
 
     async def get_records(self, entity: FeedMonitorEntity, session: aiohttp.ClientSession) -> Sequence[YoutubeFeedRecord]:
@@ -179,7 +162,7 @@ class FeedMonitor(GenericRSSMonitor):
 
     async def get_new_records(self, entity: FeedMonitorEntity, session: aiohttp.ClientSession) -> Sequence[YoutubeFeedRecord]:
         records = await self.get_records(entity, session)
-        new_records = []
+        rescheduled_records: List[YoutubeFeedRecord] = []
 
         # record.check_scheduled() involves loading video page, so it should only be done when necessarily
         # and here seems to be the only good place for it, since network request requires "session" object
@@ -189,6 +172,9 @@ class FeedMonitor(GenericRSSMonitor):
                 await record.check_scheduled(session, self.logger)
                 continue
             if not entity.track_reschedule:
+                continue
+            if not isinstance(previous, YoutubeFeedRecord):
+                self.logger.warning(f'[{entity.name}] previous version of record "{record!r}" is not YoutubeFeedRecord: {previous.model_dump()}')
                 continue
             if previous is not None and previous.scheduled is not None:
                 self.logger.debug(f'{record.video_id=} has last {previous.scheduled=}, updating')
@@ -201,46 +187,17 @@ class FeedMonitor(GenericRSSMonitor):
                     msg = 'In feed {} record [{}] {} rescheduled back from {} to {}'
                     msg = msg.format(entity.name, record.video_id, record.title, previous.scheduled, record.scheduled)
                     self.logger.warning(msg)
-                    new_records.append(record)
+                    rescheduled_records.append(record)
 
-        new_records.extend(self.filter_new_records(records, entity))
-        return new_records
+        new_records = self.filter_new_records(records, entity)
+        rescheduled_records.extend(new_records)
+        return rescheduled_records
 
     def get_record_id(self, record: YoutubeFeedRecord) -> str:
         return record.video_id
 
-    def _get_record_id(self, record: YoutubeFeedRecord, entity: FeedMonitorEntity) -> str:
-        return self.get_record_id(record)
-
-    def record_got_updated(self, record: YoutubeFeedRecord, entity: FeedMonitorEntity) -> bool:
-        return self.db.row_exists(record.video_id) and not self.db.row_exists(record.video_id, record.updated)
-
-    def store_records(self, records: Sequence[YoutubeFeedRecord], entity: FeedMonitorEntity):
-        rows = []
-        for record in records:
-            now = datetime.now(tz=timezone.utc).isoformat(timespec='seconds')
-            additional_fields = {'feed_name': entity.name, 'parsed_at': now}
-            row = record.as_dict(additional_fields)
-            rows.append(row)
-        self.db.store(rows)
-
-    def load_record(self, record: YoutubeFeedRecord, entity: FeedMonitorEntity) -> Optional[YoutubeFeedRecord]:
-        raw_latest_row = self.db.fetch_row(record.video_id)
-        if raw_latest_row is None:
-            return None
-        latest_row = dict(raw_latest_row)
-        for field in ['parsed_at', 'feed_name']:
-            latest_row.pop(field)
-        latest_row['url'] = latest_row.pop('link')
-        for field in ['published', 'updated', 'scheduled']:
-            value = latest_row[field]
-            # handle db records with old format
-            if isinstance(value, str):
-                latest_row[field] = datetime.fromisoformat(value)
-        return YoutubeFeedRecord(**latest_row)
-
     def _parse_entry(self, entry: feedparser.FeedParserDict) -> YoutubeFeedRecord:
-        parsed = {}
+        parsed: Dict[str, Any] = {}
         parsed['url'] = entry['link']
         parsed['title'] = entry['title']
         parsed['updated'] = utils.make_datetime(entry['updated_parsed'])
@@ -264,3 +221,113 @@ class FeedMonitor(GenericRSSMonitor):
         parsed['views'] = views
         record = YoutubeFeedRecord(**parsed)
         return record
+
+
+class Migration:
+    """Convert db produced by RSS2JBR in format used by RecordDB"""
+
+    name: str = 'rss2jbr'
+    table_name = 'records'
+    target_table_structure = 'parsed_at datetime, feed_name text, uid text, hashsum text, class_name text, as_json text, PRIMARY KEY(uid, hashsum)'
+    target_row_structure = ':parsed_at, :feed_name, :uid, :hashsum, :class_name, :as_json'
+
+    migrated_table_schema = '''CREATE TABLE "records" ( "parsed_at" datetime, "feed_name" text, "author" text, "video_id" text, "link" text, "title" text, "summary" text, "published" datetime, "updated" datetime, "scheduled" datetime DEFAULT NULL, "views" intefer, PRIMARY KEY("video_id","updated"))'''
+
+    def __init__(self, db_path: Union[str, Path]):
+        self.logger = logging.getLogger('migration').getChild(self.name)
+        dt = datetime.now(tz=timezone.utc).strftime('%Y_%m_%d_%H_%M_%S_%f')
+        self.backup_table_name = f'migration_{self.table_name}_{dt}'
+        if db_path == ':memory:':
+            return
+        if not Path(db_path).exists():
+            raise ValueError('db_path does not exist')
+        try:
+            self.db = sqlite3.connect(db_path, isolation_level=None)
+            self.db.row_factory = sqlite3.Row
+        except sqlite3.OperationalError as e:
+            self.logger.error(f'error opening sqlite database at path "{db_path}": {e}')
+            raise
+        else:
+            self.logger.debug(f'successfully connected to sqlite database at "{db_path}"')
+
+        with self.db:
+            self.db.execute('BEGIN TRANSACTION')
+
+            if not self.is_valid_target(self.table_name):
+                self.logger.info(f'no migration is applicable for table "{self.table_name}" at "{db_path}"')
+                return
+
+            self.logger.info(f'running migration for table "{self.table_name}" at "{db_path}"')
+            self.migrate()
+            self.logger.info(f'migration for table "{self.table_name}" at "{db_path}" completed."'
+                             f'"Original data are preserved at table "{self.backup_table_name}"')
+
+    def migrate(self):
+        self.rename_table(self.table_name, self.backup_table_name)
+        self.init_target_table(self.table_name)
+        for row in self.fetch_all_rows(self.backup_table_name):
+            converted_row = self.transform(row)
+            self.store(converted_row)
+
+    def get_table_schema(self, table_name) -> Optional[str]:
+        sql = 'SELECT sql FROM sqlite_schema WHERE name = :table_name'
+        keys = {'table_name': table_name}
+        cursor = self.db.execute(sql, keys)
+        row = cursor.fetchone()
+        if row is None:
+            return None
+        return row['sql']
+
+    def is_valid_target(self, table_name: str) -> bool:
+        """returns True if this migration can be applied to given database"""
+        existing_schema = self.get_table_schema(table_name) or ''
+        return re.sub('\s', '', self.migrated_table_schema) == re.sub('\s', '', existing_schema)
+
+    def rename_table(self, old: str, new: str):
+        sql = f'ALTER TABLE {old} RENAME TO {new}'
+        self.db.execute(sql)
+
+    def init_target_table(self, table_name: str):
+        self.db.execute('CREATE TABLE IF NOT EXISTS {} ({})'.format(table_name, self.target_table_structure))
+
+    def fetch_all_rows(self, table_name: str) -> Iterator[sqlite3.Row]:
+        sql = f'SELECT * FROM {table_name}'
+        cursor = self.db.execute(sql)
+        while True:
+            row = cursor.fetchone()
+            if row is None:
+                break
+            yield row
+
+    @staticmethod
+    def load_record(row: sqlite3.Row) -> YoutubeFeedRecord:
+        row = dict(row)
+        for field in ['parsed_at', 'feed_name']:
+            row.pop(field)
+        row['url'] = row.pop('link')
+        for field in ['published', 'updated', 'scheduled']:
+            value = row[field]
+            # handle db records with old format
+            if isinstance(value, str):
+                row[field] = datetime.fromisoformat(value)
+        return YoutubeFeedRecord(**row)
+
+    def transform(self, row: sqlite3.Row) -> Optional[Dict[str, Any]]:
+        """transform row in existing format into a row in target format"""
+        record = self.load_record(row)
+
+        transformed_row = {
+            'parsed_at': row['parsed_at'],
+            'feed_name': row['feed_name'],
+            'uid': '{}:{}'.format(row['feed_name'], row['video_id']),
+            'hashsum': record.hash(),
+            'class_name': record.__class__.__name__,
+            'as_json': record.as_json(),
+        }
+        return transformed_row
+
+    def store(self, rows: Union[Dict[str, Any], List[Dict[str, Any]]]) -> None:
+        sql = "INSERT OR IGNORE INTO {} VALUES({})".format(self.table_name, self.target_row_structure)
+        if not isinstance(rows, list):
+            rows = [rows]
+        self.db.executemany(sql, rows)
