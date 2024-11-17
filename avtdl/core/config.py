4a2bb35817c0265f3ddbbd3d76d0b1ae1434607c
user <user@localhost>
2024-11-17 23:53:50 +0300

    In SettingsSection change default value for log_directory from Path to str Also change type from Path to DirectoryPath
    Pydantic refuses to include default value with Path type into json schema

--- avtdl/core/config.py
+++ avtdl/core/config.py
@@ -1,9 +1,8 @@
 import logging
 from functools import wraps
-from pathlib import Path
 from typing import Any, Dict, Generic, List, Tuple, Type, TypeVar
 
-from pydantic import BaseModel, ConfigDict, Field, ValidationError, create_model
+from pydantic import BaseModel, ConfigDict, DirectoryPath, Field, ValidationError, create_model
 
 from avtdl.core.chain import Chain, ChainConfigSection
 from avtdl.core.interfaces import Actor
@@ -40,7 +39,7 @@ def try_parsing(func):
 class SettingsSection(BaseModel):
     model_config = ConfigDict(use_attribute_docstrings=True)
 
-    log_directory: Path = Path('logs')
+    log_directory: DirectoryPath = Field(default='logs', validate_default=True)
     """path to a directory where application will write log file"""
     logfile_size: int = Field(gt=0, default=1000000)
     """size of a single log file in bytes. After reaching this size the file will be replaced by a new one. Only last 10 files are kept inside the log directory"""
