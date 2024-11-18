cb89cf6e13964adb5514ebf375787768c8279a15
user <user@localhost>
2024-11-18 05:55:02 +0300

    In loggers, remove possible existing handlers when adding new ones

--- avtdl/core/loggers.py
+++ avtdl/core/loggers.py
@@ -14,33 +14,55 @@ class LogLevel(str, Enum):
     error = 'ERROR'
 
 
+class LoggingConfig:
+    stream_handler = None
+    file_handler = None
+
+
+    @classmethod
+    def set_logging_format(cls, level):
+        log_format = '%(asctime)s.%(msecs)03d [%(levelname)-7s] [%(name)s] %(message)s'
+        datefmt = '%Y/%m/%d %H:%M:%S'
+        root = logging.getLogger()
+        if cls.stream_handler is not None:
+            root.removeHandler(cls.stream_handler)
+        cls.stream_handler = logging.StreamHandler()
+        cls.stream_handler.setLevel(level)
+        formatter = logging.Formatter(log_format, datefmt)
+        cls.stream_handler.setFormatter(formatter)
+        root.addHandler(cls.stream_handler)
+        root.name = 'avtdl'
+        root.setLevel(logging.NOTSET)
+
+    @classmethod
+    def set_file_logger(cls, path: Path, name: str = 'avtdl.log', max_size=1000000, level: LogLevel = LogLevel.debug):
+        check_dir(path, create=True)
+        path /= name
+        log_format = '%(asctime)s.%(msecs)03d [%(levelname)s] [%(name)s] %(message)s'
+        datefmt = '%Y/%m/%d %H:%M:%S'
+        formatter = logging.Formatter(log_format, datefmt)
+        try:
+            handler = logging.handlers.RotatingFileHandler(path, maxBytes=max_size, backupCount=10, encoding='utf8')
+        except Exception as e:
+            logging.error(f'writing log to {path.absolute()} failed: {e}')
+            return
+        handler.setFormatter(formatter)
+        handler.setLevel(getattr(logging, level))
+
+        root = logging.getLogger()
+        if cls.file_handler is not None:
+            root.removeHandler(cls.file_handler)
+        cls.file_handler = handler
+        root.addHandler(cls.file_handler)
+        logging.info(f'writing verbose log to file {path.absolute()}')
+
+
 def set_logging_format(level):
-    log_format = '%(asctime)s.%(msecs)03d [%(levelname)-7s] [%(name)s] %(message)s'
-    datefmt = '%Y/%m/%d %H:%M:%S'
-    handler = logging.StreamHandler()
-    handler.setLevel(level)
-    formatter = logging.Formatter(log_format, datefmt)
-    handler.setFormatter(formatter)
-    logging.getLogger().addHandler(handler)
-    
-    logging.getLogger().name = 'avtdl'
-    logging.getLogger().setLevel(logging.NOTSET)
+    LoggingConfig.set_logging_format(level)
+
 
 def set_file_logger(path: Path, name: str = 'avtdl.log', max_size=1000000, level: LogLevel = LogLevel.debug):
-    check_dir(path, create=True)
-    path /= name
-    log_format = '%(asctime)s.%(msecs)03d [%(levelname)s] [%(name)s] %(message)s'
-    datefmt = '%Y/%m/%d %H:%M:%S'
-    formatter = logging.Formatter(log_format, datefmt)
-    try:
-        handler = logging.handlers.RotatingFileHandler(path, maxBytes=max_size, backupCount=10, encoding='utf8')
-    except Exception as e:
-        logging.error(f'writing log to {path.absolute()} failed: {e}')
-        return
-    handler.setFormatter(formatter)
-    handler.setLevel(getattr(logging, level))
-    logging.getLogger().addHandler(handler)
-    logging.info(f'writing verbose log to file {path.absolute()}')
+    LoggingConfig.set_file_logger(path, name, max_size, level)
 
 def set_logger(name: str, level: Union[LogLevel, int], propagate=True):
     if isinstance(level, LogLevel):
