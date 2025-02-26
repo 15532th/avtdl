d5af9cf64a46ddff22b530676d6dc722ef01b14d
user <user@localhost>
2025-02-26 05:48:39 +0300

    WIP on refactoring network requests: return custom Response

--- avtdl/core/request.py
+++ avtdl/core/request.py
@@ -1,37 +1,20 @@
-import asyncio
-import datetime
 import json
 import logging
 from dataclasses import dataclass
-from typing import Any, Dict, Optional, Tuple
+from http.cookies import SimpleCookie
+from typing import Any, Dict, Optional
 
 import aiohttp
+from aiohttp.abc import AbstractCookieJar
 from multidict import CIMultiDictProxy
 
-from avtdl.core.utils import Delay, get_cache_ttl, get_retry_after, utcnow
+from avtdl.core.utils import Delay, get_cache_ttl, get_retry_after
 
 HIGHEST_UPDATE_INTERVAL: float = 4000
 
 
 @dataclass
-class RequestSettings:
-    adjust_update_interval: bool = True
-    """use value from response Cache-Control or Expires headers to adjust update frequency"""
-
-    base_update_interval: float = 1
-    """normal update rate, used to calculate exponential backoff of delay on network failures"""
-    wait_before_update: bool = True
-    """make next call to request block until currently pending delay expires"""
-
-    with_cache_headers: bool = True
-    """add "If-Modified-Since" and "Etag" headers set by response to previous request to specific endpoint"""
-    none_if_unmodified: bool = True
-    """return None if response status code is 304 Not Modified"""
-    raise_for_status: bool = True
-    """raise exception on 4XX and 5XX response"""
-    raise_errors: bool = False
-    """let exceptions caused by network failures and error status codes propagate. When disabled return None instead"""
-
+class RetrySettings:
     retry_times: int = 1
     """transparent retrying: number of attempts"""
     retry_delay: float = 1
@@ -40,88 +23,85 @@ class RequestSettings:
     """transparent retrying: factor to increase retry delay compared to the previous attempt"""
 
 
-DEFAULT_SETTINGS = RequestSettings()
-
-
 @dataclass
 class EndpointState:
-    update_interval: float
-    _update_after: datetime.datetime = datetime.datetime.now(tz=datetime.timezone.utc)
     etag: Optional[str] = None
     last_modified: Optional[str] = None
 
-    def set_update_interval(self, interval: float):
-        self.update_interval = interval
-        self._update_after = utcnow() + datetime.timedelta(seconds=interval)
 
-    def get_remaining_delay(self) -> float:
-        delta = self._update_after - utcnow()
-        delay = delta.total_seconds()
-        return max(delay, 0)
+def decide_on_update_interval(logger: logging.Logger, url: str, status: Optional[int],
+                              headers: Optional[CIMultiDictProxy[str]], current_update_interval: float,
+                              base_update_interval: float, adjust_update_interval: bool = True) -> float:
+    update_interval: float
+
+    if status is None or headers is None:  # response hasn't completed due to network error
+        update_interval = Delay.get_next(current_update_interval)
+        logger.warning(f'update interval set to {update_interval} seconds for {url}')
+        return update_interval
+
+    retry_after = get_retry_after(headers)
+    if retry_after is not None:
+        raw_header = headers.get("Retry-After")
+        logger.debug(f'got Retry-After header with value {raw_header}')
+        update_interval = max(float(retry_after), HIGHEST_UPDATE_INTERVAL)
+        logger.warning(
+            f'update interval set to {update_interval} seconds for {url} as requested by response headers')
+    elif status >= 400:
+        update_interval = max(Delay.get_next(current_update_interval), current_update_interval)
+        logger.warning(f'update interval set to {update_interval} seconds for {url}')
+    else:
+        if adjust_update_interval:
+            new_update_interval = get_cache_ttl(headers) or base_update_interval
+            new_update_interval = min(new_update_interval, 10 * base_update_interval,
+                                      HIGHEST_UPDATE_INTERVAL)  # in case ttl is overly long
+            new_update_interval = max(new_update_interval, base_update_interval)
+            if new_update_interval != current_update_interval:
+                logger.info(f'next update in {new_update_interval}')
+            update_interval = new_update_interval
+        else:
+            if current_update_interval != base_update_interval:
+                logger.info(f'restoring update interval {base_update_interval} seconds for {url}')
+            update_interval = base_update_interval
+
+    return update_interval
 
 
-class Request:
+class HttpClient:
 
     def __init__(self, logger: logging.Logger, session: aiohttp.ClientSession):
         self.logger = logger
         self.session = session
 
-        # endpoints are identified by tuple (method, url, params)
-        self.endpoints: Dict[Tuple[str, str, Optional[Dict[str, str]]], EndpointState] = {}
+    @property
+    def cookie_jar(self) -> AbstractCookieJar:
+        return self.session.cookie_jar
 
-    @staticmethod
-    def decide_on_update_delay(logger: logging.Logger, url: str, status: Optional[int],
-                               headers: Optional[CIMultiDictProxy[str]], current_update_interval: float,
-                               base_update_interval: float, adjust_update_interval: bool = True) -> float:
-        update_interval: float
+    async def request(self, url: str,
+                      params: Optional[Dict[str, str]] = None,
+                      data: Optional[Any] = None,
+                      data_json: Optional[Any] = None,
+                      headers: Optional[Dict[str, Any]] = None,
+                      method: str = 'GET',
+                      state: EndpointState = EndpointState(),
+                      settings: RetrySettings = RetrySettings()) -> Optional['Response']:
+        response = None
+        next_try_delay = settings.retry_delay
+        for attempt in range(settings.retry_times):
+            response = await self.request_once(url, params, data, data_json, headers, method, state)
+            if response is not None and response.ok:
+                break
+            next_try_delay *= settings.retry_multiplier
+        return response
 
-        if status is None or headers is None:  # response hasn't completed due to network error
-            update_interval = Delay.get_next(current_update_interval)
-            logger.warning(f'update interval set to {update_interval} seconds for {url}')
-            return update_interval
-
-        retry_after = get_retry_after(headers)
-        if retry_after is not None:
-            raw_header = headers.get("Retry-After")
-            logger.debug(f'got Retry-After header with value {raw_header}')
-            update_interval = max(float(retry_after), HIGHEST_UPDATE_INTERVAL)
-            logger.warning(
-                f'update interval set to {update_interval} seconds for {url} as requested by response headers')
-        elif status >= 400:
-            update_interval = max(Delay.get_next(current_update_interval), current_update_interval)
-            logger.warning(f'update interval set to {update_interval} seconds for {url}')
-        else:
-            if adjust_update_interval:
-                new_update_interval = get_cache_ttl(headers) or base_update_interval
-                new_update_interval = min(new_update_interval, 10 * base_update_interval,
-                                          HIGHEST_UPDATE_INTERVAL)  # in case ttl is overly long
-                new_update_interval = max(new_update_interval, base_update_interval)
-                if new_update_interval != current_update_interval:
-                    logger.info(f'next update in {new_update_interval}')
-                update_interval = new_update_interval
-            else:
-                if current_update_interval != base_update_interval:
-                    logger.info(f'restoring update interval {base_update_interval} seconds for {url}')
-                update_interval = base_update_interval
-
-        return update_interval
-
-    async def raw(self, url: str,
-                  params: Optional[Dict[str, str]] = None,
-                  data: Optional[Any] = None,
-                  data_json: Optional[Any] = None,
-                  headers: Optional[Dict[str, Any]] = None,
-                  method: str = 'GET',
-                  settings: RequestSettings = DEFAULT_SETTINGS) -> Optional[aiohttp.ClientResponse]:
-        resource_id = (method, url, params)
-        if resource_id not in self.endpoints:
-            self.endpoints[resource_id] = EndpointState(settings.base_update_interval)
-        state = self.endpoints[resource_id]
+    async def request_once(self, url: str,
+                           params: Optional[Dict[str, str]] = None,
+                           data: Optional[Any] = None,
+                           data_json: Optional[Any] = None,
+                           headers: Optional[Dict[str, Any]] = None,
+                           method: str = 'GET',
+                           state: EndpointState = EndpointState(),
+                           ) -> Optional['Response']:
         logger = self.logger
-
-        if settings.wait_before_update:
-            delay = state.get_remaining_delay()
-            await asyncio.sleep(delay)
 
         request_headers: Dict[str, Any] = headers or {}
         if self.session.headers is not None:
@@ -132,79 +112,73 @@ class Request:
             request_headers['If-None-Match'] = state.etag
 
         try:
-            text = ''
             async with self.session.request(method, url, headers=request_headers, params=params, data=data,
-                                            json=data_json) as response:
+                                            json=data_json) as client_response:
                 # fully read http response to get it cached inside ClientResponse object
                 # client code can then use it by awaiting .text() again without causing
                 # network activity and potentially triggering associated errors
-                text = await response.text()
-                if settings.raise_for_status:
-                    response.raise_for_status()
+                text = await client_response.text()
         except Exception as e:
-            response_status: Optional[int] = None
-            response_headers: Optional[CIMultiDictProxy[str]] = None
-            if isinstance(e, aiohttp.ClientResponseError):
-                logger.warning(f'got code {e.status} ({e.message}) while fetching {url}')
-                if text:
-                    logger.debug(f'response body: "{text}"')
-                response_status = e.status
-                response_headers = e.headers  # type: ignore
-            else:
-                logger.warning(f'error while fetching {url}: {e.__class__.__name__} {e}')
-            update_interval = self.decide_on_update_delay(logger, url, response_status, response_headers,
-                                                          state.update_interval, settings.base_update_interval,
-                                                          settings.adjust_update_interval)
-            state.set_update_interval(update_interval)
-            if settings.raise_errors:
-                raise
-            else:
-                return None
+            logger.warning(f'error while fetching {url}: {e.__class__.__name__} {e}')
+            return None
 
-        # some servers do not have cache headers in 304 response, so only updating on 200
-        state.last_modified = response.headers.get('Last-Modified', None)
-        state.etag = response.headers.get('Etag', None)
+        if not client_response.ok:
+            logger.warning(
+                f'got code {client_response.status} ({client_response.reason or "No reason"}) while fetching {url}')
+            if text:
+                logger.debug(f'response body: "{text}"')
+        elif client_response.status != 304:
+            # some servers do not have cache headers in 304 response, so only updating on 200
+            state.last_modified = client_response.headers.get('Last-Modified', None)
+            state.etag = client_response.headers.get('Etag', None)
 
-        cache_control = response.headers.get('Cache-control')
-        logger.debug(
-            f'Last-Modified={state.last_modified or "absent"}, ETAG={state.etag or "absent"}, Cache-control="{cache_control or "absent"}"')
+            cache_control = client_response.headers.get('Cache-control')
+            logger.debug(
+                f'Last-Modified={state.last_modified or "absent"}, ETAG={state.etag or "absent"}, Cache-control="{cache_control or "absent"}"')
 
-        update_interval = self.decide_on_update_delay(logger, url, response.status, response.headers,
-                                                      state.update_interval, settings.base_update_interval,
-                                                      settings.adjust_update_interval)
-        state.set_update_interval(update_interval)
+        response = Response.from_response(client_response, text, state, logger)
 
         return response
 
-    async def text(self, url: str,
-                   params: Optional[Dict[str, str]] = None,
-                   data: Optional[Any] = None,
-                   data_json: Optional[Any] = None,
-                   headers: Optional[Dict[str, Any]] = None,
-                   method: str = 'GET',
-                   settings: RequestSettings = DEFAULT_SETTINGS) -> Optional[str]:
-        response = await self.raw(url, params, data, data_json, headers, method, settings)
-        if response is None:
-            return None
-        return await response.text()
 
-    async def json(self, url: str,
-                   params: Optional[Dict[str, str]] = None,
-                   data: Optional[Any] = None,
-                   data_json: Optional[Any] = None,
-                   headers: Optional[Dict[str, Any]] = None,
-                   method: str = 'GET',
-                   settings: RequestSettings = DEFAULT_SETTINGS) -> Optional[str]:
-        response = await self.raw(url, params, data, data_json, headers, method, settings)
-        if response is None:
-            return None
-        text = await response.text()
+@dataclass
+class Response:
+    logger: logging.Logger
+    text: str
+    url: str
+    ok: bool
+    status: int
+    reason: str
+    headers: CIMultiDictProxy[str]
+    cookies: SimpleCookie
+    endpoint_state: EndpointState
+
+    @classmethod
+    def from_response(cls, response: aiohttp.ClientResponse, text: str, state: EndpointState, logger: logging.Logger):
+        response = cls(
+            logger,
+            text,
+            str(response.url),
+            response.ok,
+            response.status,
+            response.reason or 'No reason',
+            response.headers,
+            response.cookies,
+            state
+        )
+        return response
+
+    def json(self, raise_errors: bool = False) -> Optional[Any]:
         try:
-            parsed = json.loads(text)
+            parsed = json.loads(self.text)
             return parsed
         except json.JSONDecodeError as e:
-            self.logger.debug(f'error parsing response from {url}: {e}. Raw response data: "{text}"')
-            if settings.raise_errors:
+            self.logger.debug(f'error parsing response from {self.url}: {e}. Raw response data: "{self.text}"')
+            if raise_errors:
                 raise
             else:
                 return None
+
+    def next_update_interval(self, base: float, current: float, adjust_update_interval: bool = True) -> float:
+        return decide_on_update_interval(self.logger, self.url, self.status, self.headers, current, base,
+                                         adjust_update_interval)
